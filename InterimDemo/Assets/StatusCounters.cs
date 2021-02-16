8f699a69cdd63107fee1a1a91fc6782423760259
KyleHeffernan <48320299+KyleHeffernan@users.noreply.github.com>
2021-02-16 17:07:04 +0000

    Multiple Status counters implemented

---
+++ InterimDemo/Assets/StatusCounters.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class StatusCounters : MonoBehaviour
+{
+
+
+    public TextMeshProUGUI exposedText;
+    public TextMeshProUGUI healthyText;
+    public TextMeshProUGUI infectiousText;
+
+    public Slider timeSlider;
+
+    public TextMeshProUGUI timeScaleValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject[] exposed = GameObject.FindGameObjectsWithTag("Exposed");
+        exposedText.text="Exposed: " + exposed.Length;
+
+        GameObject[] healthy = GameObject.FindGameObjectsWithTag("Healthy");
+        healthyText.text="Healthy: " + healthy.Length;
+
+        GameObject[] infectious = GameObject.FindGameObjectsWithTag("Infectious");
+        infectiousText.text="Infectious: " + infectious.Length;
+
+        timeScaleValue.text = "Time Scale: " + timeSlider.value;
+
+        
+    }
+}
