476351354292472564b9606fa594c3cba6108c7d
KyleHeffernan <48320299+KyleHeffernan@users.noreply.github.com>
2021-03-13 16:13:05 +0000

    Trying to fix contaminated areas

--- InterimDemo/Assets/ParticleCollision.cs
+++ InterimDemo/Assets/ParticleCollision.cs
@@ -7,6 +7,10 @@ public class ParticleCollision : MonoBehaviour
     public Material Exposed;
     public ParticleSystem particle;
     public List<ParticleCollisionEvent> collisionEvents;
+
+    //private List<Material> myMaterials;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -34,15 +38,38 @@ public class ParticleCollision : MonoBehaviour
             else
             {
                 other.GetComponent<MeshRenderer>().material = Exposed;
+                other.GetComponentInChildren<MeshRenderer>().material = Exposed;
 
+                /*
+                foreach(var material in other.GetComponentInChildren<MeshRenderer>().material)
+                {
 
-                /* 
-                var myMaterials = other.GetComponent<MeshRenderer>().materials;
+                }
+                
+                myMaterials = other.GetComponent<MeshRenderer>().materials;
+                
                 foreach(var material in myMaterials)
                 {
+                    other.GetComponent<MeshRenderer>();
                     material = Exposed;
                 }
                 */
+                
+                /*
+                objRenderer = other.GetComponent<MeshRenderer>();
+                originalColor = new Color[objRenderer.materials.Length];
+        
+                foreach (Material mat in objRenderer.materials)
+                {
+                    mat = Exposed;
+                    for (i = 0; i < objRenderer.materials.Length; i++)
+                    {
+                        mat = Exposed;
+                        originalColor[i] = Exposed;
+                    }
+                }
+                */
+                
                 
             }
             i++;
