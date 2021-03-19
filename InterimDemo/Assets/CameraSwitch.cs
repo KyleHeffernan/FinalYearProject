07bcadc1c2a49a4c94cbbef3b9e531ed939312a0
KyleHeffernan <48320299+KyleHeffernan@users.noreply.github.com>
2021-03-19 16:06:21 +0000

    Added Switching between Cameras

---
+++ InterimDemo/Assets/CameraSwitch.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitch : MonoBehaviour
+{
+    public GameObject camera1;
+
+    public GameObject camera2;
+
+    public GameObject camera3;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if(Input.GetButtonDown("Switch1"))
+        {
+            camera1.SetActive(true);
+            camera2.SetActive(false);
+            camera3.SetActive(false);
+        }
+        if(Input.GetButtonDown("Switch2"))
+        {
+            camera1.SetActive(false);
+            camera2.SetActive(true);
+            camera3.SetActive(false);
+
+        }
+        if(Input.GetButtonDown("Switch3"))
+        {
+            camera1.SetActive(false);
+            camera2.SetActive(false);
+            camera3.SetActive(true);
+
+        }
+
+
+    }
+}
