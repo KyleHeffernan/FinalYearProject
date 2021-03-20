18a00a1d6bc7c8001f124b3f80d8f5a955166337
KyleHeffernan <48320299+KyleHeffernan@users.noreply.github.com>
2021-03-20 19:20:31 +0000

    Camera switching now switches ui panel too

--- InterimDemo/Assets/CameraSwitch.cs
+++ InterimDemo/Assets/CameraSwitch.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CameraSwitch : MonoBehaviour
 {
@@ -9,6 +10,11 @@ public class CameraSwitch : MonoBehaviour
     public GameObject camera2;
 
     public GameObject camera3;
+
+    public GameObject panel1;
+
+    public GameObject panel2;
+
     
     // Start is called before the first frame update
     void Start()
@@ -22,12 +28,16 @@ public class CameraSwitch : MonoBehaviour
         
         if(Input.GetButtonDown("Switch1"))
         {
+            panel1.SetActive(true);
+            panel2.SetActive(false);
             camera1.SetActive(true);
             camera2.SetActive(false);
             camera3.SetActive(false);
         }
         if(Input.GetButtonDown("Switch2"))
         {
+            panel1.SetActive(false);
+            panel2.SetActive(true);
             camera1.SetActive(false);
             camera2.SetActive(true);
             camera3.SetActive(false);
@@ -35,6 +45,8 @@ public class CameraSwitch : MonoBehaviour
         }
         if(Input.GetButtonDown("Switch3"))
         {
+            panel1.SetActive(false);
+            panel2.SetActive(true);
             camera1.SetActive(false);
             camera2.SetActive(false);
             camera3.SetActive(true);
