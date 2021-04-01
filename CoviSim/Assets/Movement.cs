67ff3ba8eda64c12e3bf3c218053561fc98051d1
KyleHeffernan <48320299+KyleHeffernan@users.noreply.github.com>
2021-04-01 14:03:02 +0100

    Finally fixed movement bug

--- CoviSim/Assets/Movement.cs
+++ CoviSim/Assets/Movement.cs
@@ -15,13 +15,10 @@ public class Movement : MonoBehaviour
     CharacterController characterController;
 
     public float moveSpeed = 0.0f;
-    public float jumpSpeed = 8.0f;
     public float gravity = 20.0f;
     public float rotSpeed = 180.0f;
     
     public float walkSpeed = 6.0f;
-    public float RunSpeed = 14.0f;
-    public float maxSpeed = 0.0f;
 
     private Vector3 moveDirection = Vector3.zero;
     private Vector3 rotation;
@@ -35,48 +32,16 @@ public class Movement : MonoBehaviour
 
     void FixedUpdate()
     {
-        this.rotation = new Vector3(0, Input.GetAxisRaw("Horizontal") * rotSpeed * (Time.unscaledDeltaTime), 0);//get new rotation
-        if (characterController.isGrounded)
-        {
-            moveDirection = new Vector3(0.0f, 0.0f, Input.GetAxis("Vertical"));
-            moveDirection = this.transform.TransformDirection(moveDirection) * moveSpeed * Time.unscaledDeltaTime;
-
-            if (Input.GetAxis("Vertical") != 0)//If input then move
-            {
-                if (Input.GetKey(KeyCode.LeftShift))
-                {
-                    maxSpeed = RunSpeed;
-                }
-                else
-                {
-                    maxSpeed = walkSpeed;
-                }
-                
-                if(moveSpeed < maxSpeed)//get faster untill you reach desired speed
-                {
-                    moveSpeed += 1.0f;//get faster
-                }
-                else if (moveSpeed > maxSpeed)
-                {
-                    moveSpeed -= 1.0f;
-                }
-            }
-            else//if stop pressing then slow down
-            {
-                if(moveSpeed > 0.0f || moveSpeed > maxSpeed)//stop slowing at 0
-                {
-                    moveSpeed = 0.0f;
-                }
-            }
+        Vector3 moveRotation = new Vector3(0, Input.GetAxisRaw("Horizontal") * rotSpeed, 0);//get new rotation
+        
+        moveDirection = new Vector3(0.0f, 0.0f, Input.GetAxis("Vertical"));
+        moveDirection = this.transform.TransformDirection(moveDirection) * walkSpeed * Time.deltaTime;
             
-        }
-        // Apply gravity. Gravity is multiplied by deltaTime twice (once here, and once below
-        // when the moveDirection is multiplied by deltaTime). This is because gravity should be applied
-        // as an acceleration (ms^-2)
-        moveDirection.y -= gravity * Time.unscaledDeltaTime;
+        
+        
 
         // Move the controller
-        characterController.Move(moveDirection * Time.unscaledDeltaTime);
-        this.transform.Rotate(this.rotation * Time.unscaledDeltaTime);
+        characterController.Move(moveDirection * (Time.deltaTime * 1 / Time.timeScale));
+        this.transform.Rotate(moveRotation * (Time.deltaTime * 1 / Time.timeScale));
     }//End update
 }
